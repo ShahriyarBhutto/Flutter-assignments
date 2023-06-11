ea234a359d5b0ff0f1fc9f1739f2d64c04ae76e7
ShahriyarBhutto <88304298+ShahriyarBhutto@users.noreply.github.com>
2023-06-11 13:46:32 +0500

    first commit

---
+++ index.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int num = 18;
+  int num2 = 10;
+
+  if (num2 > num) {
+    print('num one is grreater ${num}');
+  } else {
+    print('num one is not graweter');
+  }
+}
